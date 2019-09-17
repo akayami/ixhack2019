43a9accebeeb34292938d018ca56d70318b8f0d9
Gabriel.Feyer <Bucuresti65>
2019-09-17 18:49:54 -0400

    - parallel python backend (BACKUP in case NLP API does not work)

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+from logger import getlogger
+import nltk
+
+logger = getlogger(__name__)
+
+app = Flask(__name__, template_folder='html/templates', static_folder='html/static')
+socketio = SocketIO(app)
+app.debug = False
+
+
+@app.route('/')
+def index():
+    return render_template('test1.html')
+
+
+@socketio.on('text')
+def handle_message(message):
+    txt = message['data']
+    is_noun = lambda pos: pos[:2] == 'NN'
+    tokenized = nltk.word_tokenize(txt)
+    nouns = [word for (word, pos) in nltk.pos_tag(tokenized) if is_noun(pos)]
+    print(txt)
+    print(nouns)
+    socketio.emit('text', nouns)
+
+
+@socketio.on('json')
+def handle_json(json):
+    print('received json: ' + str(json))
+
+
+if __name__ == '__main__':
+    logger.debug("start")
+    socketio.run(app, debug=True)
