1eaf52a8064ac7ead79e5b64cf80e0db01f38114
Andrew Walker <andrew.walker@indexexchange.com>
2019-09-18 11:13:12 -0400

    Adding ability to toggle text on and off

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 var elementCount = 0;
+var textVisible = false;
 
 $(document).ready(async function(){
 	for (i = 0; i < 10; i++) {
@@ -7,9 +8,12 @@ $(document).ready(async function(){
 		// Random height between 200 and 600
 		var newH = 200 + parseInt(Math.random() * 4) * 100;
 
-		var newElement = createImage(newY, newH, "https://s.abcnews.com/images/Lifestyle/puppy-ht-3-er-170907_4x3_992.jpg");
+		var newElement = createImage(newY, newH, "https://s.abcnews.com/images/Lifestyle/puppy-ht-3-er-170907_4x3_992.jpg", "puppy-"+newY);
 		animateElement(newElement);
 		await sleep(1000);
+		if (elementCount % 4 == 0) {
+			changeTextMode(!textVisible);
+		}
 	}
 });
 
@@ -17,25 +21,45 @@ function sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function createImage(y, height, src) {
-	elementCount++;
+function changeTextMode(textMode) {
+	textVisible = textMode;
+
+	applyCurrentTextVisibility($('.text'));
+}
+
+function applyCurrentTextVisibility(element) {
+	if(textVisible) {
+		element.css("visibility", "visible");
+	} else {
+		element.css("visibility", "hidden");
+	}
+}
+
+function createImage(y, height, src, text) {
 	var newID = "thing" + elementCount;
 
-	var div = $('<div>');
+	var div = $('<div class="container movable' + y + '">');
 	div.attr("id", newID);
 	div.attr("z-index", elementCount);
-	div.appendTo("body");
+	div.appendTo("#parent");
+
+	var textDiv = $('<div id="textDiv" class="text top-right">' + text + '</div>');
+	textDiv.attr("z-index", elementCount + 1);
+	textDiv.appendTo("#" + newID);
+	applyCurrentTextVisibility(textDiv);
 	
-	var img = $('<img class="movable' + y + '">');
+	var img = $('<img>');
 	img.attr('src', src);
 	img.attr("height", height);
 	img.appendTo("#" + newID);
+
+	elementCount += 2;
 
 	return newID;
 }
 
 function animateElement(id) {
-	$("#" + id).addClass("flier");
+	// $("#" + id).addClass("flier");
   
 	$("#" + id).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
 		$(this).remove();
