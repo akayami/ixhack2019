65e92fbf56a30661114cf6b756440f12ad8630c4
Gabriel.Feyer <Bucuresti65>
2019-09-18 09:49:09 -0400

    - python backend - flickr api

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 from logger import getlogger
 import nltk
+from flickrapi import FlickrAPI
+import json
 
 logger = getlogger(__name__)
 
@@ -9,10 +11,32 @@ app = Flask(__name__, template_folder='html/templates', static_folder='html/stat
 socketio = SocketIO(app)
 app.debug = False
 
+flickr = FlickrAPI('c6a2c45591d4973ff525042472446ca2', '202ffe6f387ce29b', format='parsed-json')
+
 
 @app.route('/')
 def index():
     return render_template('test1.html')
+
+
+def query_flickr(nouns):
+    if nouns:
+        extras = 'url_n'
+        query = " ".join(nouns)
+        print("query= " + query)
+
+        max = 9
+        results = flickr.photos.search(text=query, per_page=max, extras=extras)
+        pics = []
+
+        for i in range(0, max):
+            try:
+                pics.append(results['photos']['photo'][i]['url_n'])
+                print(pics)
+            except Exception as ex:
+                pass
+
+        return pics
 
 
 @socketio.on('text')
@@ -24,6 +48,11 @@ def handle_message(message):
     nouns = [word for (word, pos) in nltk.pos_tag(tokenized) if is_noun(pos)]
     print(nouns)
     socketio.emit('text', nouns)
+
+    pics = query_flickr(nouns)
+
+    if pics:
+        socketio.emit('pics', pics)
 
 
 @socketio.on('json')
