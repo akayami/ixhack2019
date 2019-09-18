28a0aa4566b922043efb2ad15e0507348b692872
Tomasz Rakowski <akayami@gmail.com>
2019-09-18 11:22:21 -0400

    Parallel source querying

--- public/app.js
+++ public/app.js
@@ -1,8 +1,8 @@
 var elementCount = 0;
 
-$(document).ready(async function(){
+var state = {};
 
-	var f = document.getElementById('body');
+$(document).ready(async function(){
 
 	var recognition = new webkitSpeechRecognition();
 	recognition.continuous = true;
@@ -14,29 +14,111 @@ $(document).ready(async function(){
 			o.push(event.results[i][0].transcript);
 		}
 		var part = o.pop();
-		console.log(part);
-		f.append(generate(part));
+		//console.log(part);
+		generate(part);
 	};
 	recognition.start()
 });
 
-async function addToDom(picJSON) {
+function randomIntFromInterval(min, max) { // min and max included
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+async function addToDom(list) {
 //	console.log(picJSON)
-	for (var i = 0; i < (picJSON.hits.length >= 3 ? 3 : picJSON.hits.length); i++) {
+	for (var i = 0; i < (list.length >= 3 ? 3 : list.length); i++) {
 		var newY = parseInt(Math.random() * 8) * 10;
 		// Random height between 200 and 600
 		var newH = 200 + parseInt(Math.random() * 4) * 100;
 
-		var newElement = createImage(newY, newH, picJSON.hits[i].webformatURL);
+		var newElement = createImage(newY, newH, list[i].url);
 		animateElement(newElement);
 		await sleep(1000);
 	}
 }
 
 function generate(input) {
+	Promise.all([
+		new Promise((resolve, reject) => {
+			pixabay(input, (err, data) => {
+				resolve(parsePixabay(err, data));
+			});
+		}),
+		new Promise((resolve, reject) => {
+			flicker(input, (err, data) => {
+				resolve(parseFlicker(err, data));
+			});
+		})
+	]).then((values) => {
+//		console.log(values);
+		var all = [];
+		for(var i = 0; i < values.length; i++) {
+			//console.log(values[i]);
+			for(var z = 0; z < values[i].length; z++) {
+				all.push(values[i][z]);
+			}
+		}
+		var output = [];
+		for(var p = 0; p < 3; p++) {
+			output.push(all[randomIntFromInterval(0, all.length)]);
+		}
+		addToDom(output);
+//		console.log(output);
+
+	}).catch((e) => {
+		console.error(e);
+	});
+	// pixabay(input, parsePixabay);
+	// flicker(input, parseFlicker);
+}
+
+function parseFlicker(err, data) {
+	console.log('Flicker lookup is done');
+	console.log(data);
+	let output = [];
+	for (var i = 0; i < data.items.length; i++) {
+		output.push({url: data.items[i].media.m});
+	}
+	return output;
+}
+
+function parsePixabay(err, input) {
+	let output = [];
+	console.log(input);
+	for (var i = 0; i < input.hits.length; i++) {
+		output.push({url: input.hits[i].webformatURL});
+	}
+	return output;
+}
+
+function pixabay1(text, cb) {
+	console.log('Sending to pixabay', text)
 	var e = document.createElement('script');
-	e.src = "https://pixabay.com/api/?key=13658839-11ca33364dfe1124291ae842d&callback=addToDom&per_page=18&q=" + encodeURI(input) + "&lang=en&orientation=horizontal"
-	return e;
+	e.src = "https://pixabay.com/api/?key=13658839-11ca33364dfe1124291ae842d&callback=parsePixabay&per_page=18&q=" + encodeURI(text) + "&lang=en&orientation=horizontal"
+	document.getElementById('body').append(e);
+	//return e;
+}
+
+function pixabay(text, cb) {
+	console.log('Flicker input', text);
+	var url = "https://pixabay.com/api/?key=13658839-11ca33364dfe1124291ae842d&callback=?&per_page=18&q=" + encodeURI(text) + "&lang=en&orientation=horizontal"
+	$.getJSON(url)
+		.done(function (data) {
+			cb(null, data);
+		})
+}
+
+function flicker(text, cb) {
+	console.log('Flicker input', text);
+	var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+	$.getJSON(flickerAPI, {
+		tags: text,
+		tagmode: "any",
+		format: "json"
+	})
+		.done(function (data) {
+			cb(null, data);
+		})
 }
 
 function sleep(ms) {
@@ -51,7 +133,7 @@ function createImage(y, height, src) {
 	div.attr("id", newID);
 	div.attr("z-index", elementCount);
 	div.appendTo("body");
-	
+
 	var img = $('<img class="movable' + y + '">');
 	img.attr('src', src);
 	img.attr("height", height);
@@ -62,7 +144,7 @@ function createImage(y, height, src) {
 
 function animateElement(id) {
 	$("#" + id).addClass("flier");
-  
+
 	$("#" + id).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
 		$(this).remove();
 	});
